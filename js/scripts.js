7a2e542da37b5d6f39bf7b0b2beca36e6abfbd5d
Dr. Lawrence Ibeh <49813088+NextParadym@users.noreply.github.com>
2021-08-24 14:38:33 +0200

    Create a Variable 'size' : 100  and double it

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,8 @@ alert('Hello world');
 // Variable
 let favoriteFood = 'rice';
 console.log(favoriteFood)
+
+// Create a Variable 'size', representing 100 and double the variable
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
