5e19903c37dfb584e6da4eb43390da907bb180d8
Dr. Lawrence Ibeh <49813088+NextParadym@users.noreply.github.com>
2021-08-29 18:13:25 +0200

    Updated the heights as integer instead of string

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,32 @@
 let pokemonList = [
   {
     name: "Charmeleon",
-    height: " 1.1",
+    height: 1.1,
     type: "fire"
   },
 
   {
     name: "Nidoking",
-    height: " 1.4",
+    height: 1.4,
     type: ["ground","poison"]
   },
 
   {
     name: "Psyduck",
-    height: " 0.8",
+    height: 0.8,
     type: "water"
   },
 
   {
     name: "Pikachu",
-    height: " 0.4",
+    height: 0.4,
     type: "Electric"
   },
 
   {
-    name: "Metapod,
-    height: " 0.7",
-    type: "Bug"
+    name: "Venusaur",
+    height: 2,
+  type: ["ground","poison"]
   }
 ];
 
