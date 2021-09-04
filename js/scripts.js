a72b2950339a0ac7a0941479611eb32ea53686cc
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-09-04 12:05:22 +0200

    Fixed Syntax, indent, ol

--- js/scripts.js
+++ js/scripts.js
@@ -6,36 +6,35 @@ let pokemonList = [
     height: 1.1,
     types: ["fire"]
   },
-
   {
     name: "Nidoking",
     height: 1.4,
     types: ["ground","poison"]
   },
-
   {
     name: "Psyduck",
     height: 0.8,
     types: ["water"]
   },
-
   {
     name: "Pikachu",
     height: 0.4,
-    types: ["Electric"|
+    types: ["Electric"]
   },
-
   {
     name: "Venusaur",
     height: 2,
     types: ["ground","poison"]
   }
 ];
-   // create a for loop to iterate over each object in the List of Pokemons Objects
-    for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.1) {
-    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + 'Wow, that’s big </p>');
+
+// create a for loop to iterate over each object in the List of Pokemons Objects
+document.write('<ol>');
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 1.0) {
+    document.write('<li>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + ' - Wow, that’s big </li>');
   } else {
-    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '</p>');
+    document.write('<li>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '</li>');
   }
 }
+document.write('</ol>');
