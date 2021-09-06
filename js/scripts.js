573cd59ccb78ccd67b58a564f9c2813894cdf6c8
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-09-06 19:32:22 +0200

    Create,wrap pokemonList in IIFE,pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,52 @@
 
-// Using forEach function intead of for loop
-let pokemonList = [
-  {
-    name: "Charmeleon",
-    height: 1.1,
-    types: ["fire"]
-  },
-  {
-    name: "Nidoking",
-    height: 1.4,
-    types: ["ground","poison"]
-  },
-  {
-    name: "Psyduck",
-    height: 0.8,
-    types: ["water"]
-  },
-  {
-    name: "Pikachu",
-    height: 0.4,
-    types: ["Electric"]
-  },
-  {
-    name: "Venusaur",
-    height: 2,
-    types: ["ground","poison"]
-  }
-];
+// pokemonRepository
+var pokemonRepository = (function (){ 
+  var pokemonList = [
+    {
+      name: "Charmeleon",
+      height: 1.1,
+      types: ["fire"]
+    },
+    {
+      name: "Nidoking",
+      height: 1.4,
+      types: ["ground","poison"]
+    },
+    {
+      name: "Psyduck",
+      height: 0.8,
+      types: ["water"]
+    },
+    {
+      name: "Pikachu",
+      height: 0.4,
+      types: ["Electric"]
+    },
+    {
+      name: "Venusaur",
+      height: 2,
+      types: ["ground","poison"]
+    }
+  ];
 
-pokemonList.forEach(function(pokemon) {
+  function getAll() {
+    return pokemonList;
+  } //returns Pokemon List
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  } //add pokemons
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
   
-});
-
+  // forEach() Loop of the pokemonList
 document.write('<ol>');
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write('<li>' +pokemon.name + ' is ' + pokemon.height+ ' by height'+ ' with ' + pokemon.types + ' as type category.'+ '</li>');
 })
 document.write('</ol>');
