7070fce1d5838812ae9f08d0879fc9c730ebe285
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-09-11 17:05:46 +0200

    Load external pokemon data-upadte scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,22 @@
 
 // pokemonRepository
-var pokemonRepository = (function () {
-  var repository = [
-    {
-      name: "Charmeleon",
-      height: 1.1,
-      types: ["fire"]
-    },
-    {
-      name: "Nidoking",
-      height: 1.4,
-      types: ["ground", "poison"]
-    },
-    {
-      name: "Psyduck",
-      height: 0.8,
-      types: ["water"]
-    },
-    {
-      name: "Pikachu",
-      height: 0.4,
-      types: ["Electric"]
-    },
-    {
-      name: "Venusaur",
-      height: 2,
-      types: ["ground", "poison"]
-    }
-  ];
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
+      "name" in pokemon
     ) {
-      repository.push(pokemon);
+      pokemonList.push(pokemon);
     } else {
       console.log("pokemon is not correct");
     }
   }
 
   function getAll() {
-    return repository;
-  }
-
-  //  showDetails function and console
-  function showDetails(pokemon) {
-    console.log(pokemon);
-    alert(pokemon.name);
+    return pokemonList;
   }
 
     //  addListItem
@@ -61,35 +28,66 @@ var pokemonRepository = (function () {
     button.classList.add("pokemon-button");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-
     // add Eventlistener
-    button.addEventListener('click', function (event) {
+    button.addEventListener('click', function(event) {
       showDetails(pokemon);
     });
+  }
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  //  showDetails function and console
+  function showDetails(pokemon) {
+     pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+       });
   }
 
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
-
 })();
 
- // add pokemon to pokemonRepository
-pokemonRepository.add({ name: "Hoothoot", height: 0.7, types: ["normal","plug"] });
-
-
  // forEach() Loop of the pokemonList
+
+pokemonRepository.loadList().then(function () {
 pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon);
-})
+  });
+});
 
 console.log(pokemonRepository.getAll());
-
- // print forEach() Loop of the pokemonList
-document.write('<ul>');
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write('<li>' +pokemon.name + ' is ' + pokemon.height+ ' by height'+ ' with ' + pokemon.types + ' as type category.'+ '</li>');
-})
-document.write('</ul>');
